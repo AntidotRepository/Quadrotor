83975706eda66247e98462f87a8faddebb7f613a
antidot <haroldducept@gmail.com>
2015-07-27 20:55:25 +0200

    Added XBee files + task using it

--- conf/main.c
+++ conf/main.c
@@ -1,8 +1,8 @@
 #include "ch.h"
 #include "hal.h"
+#include "XBee.h"
 
-static WORKING_AREA(waThread1, 128);
-static msg_t Thread1(void *arg);
+static WORKING_AREA(waComSnd, 512);
 
 int main(void)
 {
@@ -11,26 +11,12 @@ int main(void)
 	chSysInit();
 	
 	/* Create one more task */
-	chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
-	
+	chThdCreateStatic(waComSnd, sizeof(waComSnd), NORMALPRIO, ThreadComSnd, NULL);
+
 	/* Main task (always present and have priority NORMALPRIO) */
 	while(TRUE)
 	{
-		palSetPad(GPIOB, GPIOB_LED4);
-		chThdSleepMilliseconds(100);
-		palClearPad(GPIOB, GPIOB_LED4);
 		chThdSleepMilliseconds(100);
 	}
 }
 
-static msg_t Thread1(void *arg)
-{
-	/* Another task */
-	while(TRUE)
-	{
-		palSetPad(GPIOB, GPIOB_LED3);
-		chThdSleepMilliseconds(500);
-		palClearPad(GPIOB, GPIOB_LED3);
-		chThdSleepMilliseconds(500);
-	}
-}
